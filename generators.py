cc85cfaa45c17ed44bd1d7dc866a76319b6f0538
Teague Tomesh <ttomesh@princeton.edu>
2020-02-10 17:11:55 -0500

    implemented ripple-carry adder

--- generators.py
+++ generators.py
@@ -5,6 +5,7 @@ from .QFT import qft_circ
 from .QWalk import quantum_walk
 from .Dynamics import quantum_dynamics
 from .BernsteinVazirani import bernstein_vazirani
+from .Arithmetic import ripple_carry_adder
 
 def gen_supremacy(height, width, depth, order=None, singlegates=True,
                   mirror=False, barriers=False, measure=False, regname=None):
@@ -58,7 +59,7 @@ def gen_hwea(width, depth, parameters='optimal', seed=None, barriers=False,
     return circ
 
 
-def gen_uccsd(width, parameters='seeded', seed=None, barriers=True,
+def gen_uccsd(width, parameters='random', seed=None, barriers=False,
               regname=None):
     """
     Generate a UCCSD ansatz with the given width (number of qubits).
@@ -128,6 +129,26 @@ def gen_BV(secret=None, barriers=True,  measure=False, regname=None):
     circ = bv.gen_circuit()
 
     return circ
+
+
+def gen_adder(nbits=None, a=0, b=0, use_toffoli=False, barriers=True,
+              measure=False, regname=None):
+    """
+    Generate an n-bit ripple-carry adder which performs a+b and stores the
+    result in the b register.
+
+    Based on the implementation of: https://arxiv.org/abs/quant-ph/0410184v1
+    """
+
+    adder = ripple_carry_adder.RCAdder(nbits=nbits, a=a, b=b,
+                use_toffoli=use_toffoli, barriers=barriers, measure=measure,
+                regname=regname)
+
+    circ = adder.gen_circuit()
+
+    return circ
+
+
 
 
 
